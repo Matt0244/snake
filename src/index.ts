74089fc43dba7c7d9fd7c7aabb218c29bf0171f8
Matt0244 <87352633+Matt0244@users.noreply.github.com>
2024-10-23 11:11:21 +1030

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import "./style/index.less"
+import GameControl from "./modules/GameControl";
+// import Food from "./modules/Food";
+// import Snake from "./modules/Snake";
+// import ScorePanel from "./modules/ScorePanel";
+
+
+
+
+new GameControl ();
